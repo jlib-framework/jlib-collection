79d8741de9d4df2b02e081c956191ae203558671
Igor Akkerman <igor.akkerman@jlib.org>
2016-05-19 23:36:41 +0200

    additional Lombok usage

--- src/main/java/org/jlib/collection/CachingList.java
+++ src/main/java/org/jlib/collection/CachingList.java
@@ -29,6 +29,7 @@ import java.util.ListIterator;
 import java.util.Spliterator;
 import java.util.function.UnaryOperator;
 
+import lombok.RequiredArgsConstructor;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
@@ -79,6 +80,7 @@ import org.checkerframework.checker.nullness.qual.Nullable;
  *
  * @author Igor Akkerman
  */
+@RequiredArgsConstructor
 public final class CachingList<Item>
     implements List<Item> {
 
@@ -90,16 +92,6 @@ public final class CachingList<Item>
 
     /** last looked up {@link Item}; {@code null} if unset (for performance reasons) */
     private @Nullable Object lastLookedUpItem;
-
-    /**
-     * Creates a new {@link CachingList}.
-     *
-     * @param delegate
-     *        delegate {@link List} to which all calls are delegated
-     */
-    public CachingList(final List<Item> delegate) {
-        this.delegate = delegate;
-    }
 
     @Override
     public boolean contains(final @Nullable Object item) {
